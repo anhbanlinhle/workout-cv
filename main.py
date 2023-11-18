64577c71df91ba04472035f56eaf79fa2ced01e9
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2023-11-18 15:31:25 +0700

    update constrant and main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from models import count_pushup
+from push_up import count_pushup_angle
+from constant import RIGHT_SHOULDER
+from constant import RIGHT_ELBOW
+from constant import RIGHT_WRIST
+from constant import LEFT_SHOULDER
+from constant import LEFT_ELBOW
+from constant import LEFT_WRIST
 
-path = "path/to/video"
 
-count = count_pushup(path)
-
+path = "data/cd5.mp4"
+# imlist = create_imlist(path)
+count = count_pushup_angle(path)
 print(count)
+
