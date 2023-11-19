4ec8f1c21272389abb606635ed427a96f50ac2c1
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2023-11-19 19:41:49 +0700

    Update push_up.py

--- push_up.py
+++ push_up.py
@@ -1,11 +1,7 @@
 import cv2
-import mediapipe as md
-import time
-import math
-import moviepy.video.fx.all as vfx
+import mediapipe as mp
+import numpy as np
 import constant
-from moviepy.editor import VideoFileClip
-
 from constant import RIGHT_SHOULDER
 from constant import RIGHT_ELBOW
 from constant import RIGHT_WRIST
@@ -18,127 +14,66 @@ from constant import LEFT_WRIST
 from constant import LEFT_HIP
 from constant import LEFT_KNEE
 from constant import LEFT_ANKLE
-
-
 def getAngle(a, b, c):
-    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
-    return ang + 360 if ang < 0 else ang
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
 
-def create_imlist(path):
-    md_pose = md.solutions.pose 
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
+        np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
 
-    count = 0
-    position = None 
+    if angle > 180.0:
+        angle = 360-angle
 
-    cap = cv2.VideoCapture(path)
-    imlist = []
-    # debug
-    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    start_time = time.time()
+    return angle
+        
+        
 
-    with md_pose.Pose(
-        min_detection_confidence = 0.7,
-        min_tracking_confidence = 0.7
-    ) as pose:
-        while cap.isOpened():
-            success, image = cap.read()
-            if not success:
-                print('Video not found or ended')
-                break
-            
-            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-            result = pose.process(image)
-            
-            # imlist = []
-
-            if result.pose_landmarks:
-                for id, lm in enumerate(result.pose_landmarks.landmark):
-                    h, w, _ = image.shape
-                    # X, Y = int(lm.x * w), int(lm.y * h)
-                    imlist.append([id, lm.x, lm.y])
-    return imlist
-
-
-def count_pushup_angle(path):
-    md_pose = md.solutions.pose  
-
-    count = 0
-    position = None 
-
-    cap = cv2.VideoCapture(path)
-    angle = []
-    # debug
-    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    start_time = time.time()
-
-    with md_pose.Pose(
-        min_detection_confidence = 0.7,
-        min_tracking_confidence = 0.7
-    ) as pose:
-        while cap.isOpened():
-            success, image = cap.read()
-            if not success:
-                print('Video not found or ended')
-                break
-            
-            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-            result = pose.process(image)
-            
-            imlist = []
-
-            if result.pose_landmarks:
-                for id, lm in enumerate(result.pose_landmarks.landmark):
-                    h, w, _ = image.shape
-                    X, Y = int(lm.x * w), int(lm.y * h)
-                    imlist.append([id, X, Y])
-            if len(imlist) != 0:
-                angle_SEW_left =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
+def SEW_left(imlist):
+    if len(imlist) != 0:
+        result =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
                                         (imlist[LEFT_ELBOW][1],imlist[LEFT_ELBOW][2]),
                                         (imlist[LEFT_WRIST][1],imlist[LEFT_WRIST][2]))
-                angle_SEW_right =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
+    
+    return result
+
+def SEW_right(imlist):
+    if len(imlist) != 0:
+        result =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
                                         (imlist[RIGHT_ELBOW][1],imlist[RIGHT_ELBOW][2]),
                                         (imlist[RIGHT_WRIST][1],imlist[RIGHT_WRIST][2]))
-                angle_SHK_left = getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
+    
+    return result
+
+def SHK_left(imlist):
+    if len(imlist) != 0:
+        result =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
                                         (imlist[LEFT_HIP][1],imlist[LEFT_HIP][2]),
                                         (imlist[LEFT_KNEE][1],imlist[LEFT_KNEE][2]))
-                angle_SHK_right = getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
+    
+    return result
+
+def SHK_right(imlist):
+    if len(imlist) != 0:
+        result =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
                                         (imlist[RIGHT_HIP][1],imlist[RIGHT_HIP][2]),
                                         (imlist[RIGHT_KNEE][1],imlist[RIGHT_KNEE][2]))
-                angle_HKA_left = getAngle((imlist[LEFT_HIP][1], imlist[LEFT_HIP][2]),
+    
+    return result
+
+def HKA_left(imlist):
+    if len(imlist) != 0:
+        result =  getAngle((imlist[LEFT_HIP][1], imlist[LEFT_HIP][2]),
                                         (imlist[LEFT_KNEE][1],imlist[LEFT_KNEE][2]),
                                         (imlist[LEFT_ANKLE][1],imlist[LEFT_ANKLE][2]))
-                angle_HKA_right = getAngle((imlist[RIGHT_HIP][1], imlist[RIGHT_HIP][2]),
+    
+    return result
+
+def HKA_right(imlist):
+    if len(imlist) != 0:
+        result =  getAngle((imlist[RIGHT_HIP][1], imlist[RIGHT_HIP][2]),
                                         (imlist[RIGHT_KNEE][1],imlist[RIGHT_KNEE][2]),
                                         (imlist[RIGHT_ANKLE][1],imlist[RIGHT_ANKLE][2]))
-                if (angle_SEW_right >= 180 and angle_SEW_left >= 180) :
-                    angle_SEW_left = 360 - getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
-                                        (imlist[LEFT_ELBOW][1],imlist[LEFT_ELBOW][2]),
-                                        (imlist[LEFT_WRIST][1],imlist[LEFT_WRIST][2]))
-                    angle_SEW_right = 360 -  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
-                                        (imlist[RIGHT_ELBOW][1],imlist[RIGHT_ELBOW][2]),
-                                        (imlist[RIGHT_WRIST][1],imlist[RIGHT_WRIST][2]))
-                    angle_SHK_left = 360 - getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
-                                        (imlist[LEFT_HIP][1],imlist[LEFT_HIP][2]),
-                                        (imlist[LEFT_KNEE][1],imlist[LEFT_KNEE][2]))
-                    angle_SHK_right = 360 - getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
-                                            (imlist[RIGHT_HIP][1],imlist[RIGHT_HIP][2]),
-                                            (imlist[RIGHT_KNEE][1],imlist[RIGHT_KNEE][2]))
-                    angle_HKA_left = 360 - getAngle((imlist[LEFT_HIP][1], imlist[LEFT_HIP][2]),
-                                            (imlist[LEFT_KNEE][1],imlist[LEFT_KNEE][2]),
-                                            (imlist[LEFT_ANKLE][1],imlist[LEFT_ANKLE][2]))
-                    angle_HKA_right = 360 - getAngle((imlist[RIGHT_HIP][1], imlist[RIGHT_HIP][2]),
-                                            (imlist[RIGHT_KNEE][1],imlist[RIGHT_KNEE][2]),
-                                            (imlist[RIGHT_ANKLE][1],imlist[RIGHT_ANKLE][2]))
-
-
-                if  ((angle_SEW_left) <= constant.SEW_THRESHOLD and (angle_SEW_right) <= constant.SEW_THRESHOLD and
-                    ((angle_SHK_left) >= constant.SHK_THRESHOLD and (angle_SHK_right) >= constant.SHK_THRESHOLD) and 
-                    ((angle_HKA_left) >= constant.HKA_THRESHOLD and (angle_HKA_right) >= constant.HKA_THRESHOLD) ):
-                    position = "down"  
-                if (((angle_SEW_left) >= constant.SEW_THRESHOLD and (angle_SEW_right) >= constant.SEW_THRESHOLD) and
-                    ((angle_SHK_left) >= constant.SHK_THRESHOLD and (angle_SHK_right) >= constant.SHK_THRESHOLD) and
-                    ((angle_HKA_left) >= constant.HKA_THRESHOLD and (angle_HKA_right) >= constant.HKA_THRESHOLD) and position == "down"):
-                    position = "up"
-                    count +=1 
-                    print(count) 
-    return count
+    
+    return result
