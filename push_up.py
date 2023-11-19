62e96015ac93c449685e5d922742a8c5801ef4fe
Viet Anh Hoang <hoangvietanh8203@gmail.com>
2023-11-19 20:25:28 +0700

    update push_up

--- push_up.py
+++ push_up.py
@@ -27,53 +27,42 @@ def getAngle(a, b, c):
         angle = 360-angle
 
     return angle
-        
-        
 
-def SEW_left(imlist):
+# SEW - Shoulder Elbow Wrist
+# SHK - Shoulder Hip Knee
+# HKA - Hip Knee Ankle
+# angle[0] = SEW_left 
+# angle[1] = SEW_right
+# angle[2] = SHK_left
+# angle[3] = SHK_right
+# angle[4] = HKA_left
+# angle[5] = HKA_right
+
+def count_push_up(imlist):
+    angle = []
     if len(imlist) != 0:
-        result =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
+        SEW_left =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
                                         (imlist[LEFT_ELBOW][1],imlist[LEFT_ELBOW][2]),
                                         (imlist[LEFT_WRIST][1],imlist[LEFT_WRIST][2]))
-    
-    return result
-
-def SEW_right(imlist):
-    if len(imlist) != 0:
-        result =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
+        angle.append(SEW_left)
+        SEW_right =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
                                         (imlist[RIGHT_ELBOW][1],imlist[RIGHT_ELBOW][2]),
                                         (imlist[RIGHT_WRIST][1],imlist[RIGHT_WRIST][2]))
-    
-    return result
-
-def SHK_left(imlist):
-    if len(imlist) != 0:
-        result =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
+        angle.append(SEW_right)
+        SHK_left =  getAngle((imlist[LEFT_SHOULDER][1], imlist[LEFT_SHOULDER][2]),
                                         (imlist[LEFT_HIP][1],imlist[LEFT_HIP][2]),
                                         (imlist[LEFT_KNEE][1],imlist[LEFT_KNEE][2]))
-    
-    return result
-
-def SHK_right(imlist):
-    if len(imlist) != 0:
-        result =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
+        angle.append(SHK_left)
+        SHK_right =  getAngle((imlist[RIGHT_SHOULDER][1], imlist[RIGHT_SHOULDER][2]),
                                         (imlist[RIGHT_HIP][1],imlist[RIGHT_HIP][2]),
                                         (imlist[RIGHT_KNEE][1],imlist[RIGHT_KNEE][2]))
-    
-    return result
-
-def HKA_left(imlist):
-    if len(imlist) != 0:
-        result =  getAngle((imlist[LEFT_HIP][1], imlist[LEFT_HIP][2]),
+        angle.append(SHK_right)
+        HKA_left =  getAngle((imlist[LEFT_HIP][1], imlist[LEFT_HIP][2]),
                                         (imlist[LEFT_KNEE][1],imlist[LEFT_KNEE][2]),
                                         (imlist[LEFT_ANKLE][1],imlist[LEFT_ANKLE][2]))
-    
-    return result
-
-def HKA_right(imlist):
-    if len(imlist) != 0:
-        result =  getAngle((imlist[RIGHT_HIP][1], imlist[RIGHT_HIP][2]),
+        angle.append(HKA_left)
+        HKA_right =  getAngle((imlist[RIGHT_HIP][1], imlist[RIGHT_HIP][2]),
                                         (imlist[RIGHT_KNEE][1],imlist[RIGHT_KNEE][2]),
                                         (imlist[RIGHT_ANKLE][1],imlist[RIGHT_ANKLE][2]))
-    
-    return result
+        angle.append(HKA_right)
+    return angle
