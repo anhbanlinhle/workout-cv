4f946db7f0909d4d068bdf71bcda943285265af5
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-19 17:45:11 +0700

    clean code

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as md
 import constant
+from drawing import draw_landmarks
 
 def process_data(path):
     md_pose = md.solutions.pose 
@@ -8,7 +9,6 @@ def process_data(path):
     count = 0
     position = None 
 
-    # cap = cv2.VideoCapture(path)
     cap = cv2.VideoCapture(0)
 
     with md_pose.Pose(
@@ -47,45 +47,10 @@ def process_data(path):
                     count +=1 
                     print(count)
 
-            cv2.imshow('Pose Landmarks', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+            cv2.imshow('Workout Scanner', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
     cap.release()
     cv2.destroyAllWindows()
     return count
-
-def draw_landmarks(md_pose, image, landmarks, landmark_indices, imlist):
-    # for id in landmark_indices:
-    #     lm = landmarks.landmark[id]
-    #     h, w, _ = image.shape
-    #     X, Y = int(lm.x * w), int(lm.y * h)
-    #     imlist.append([id, X, Y])
-    for id, lm in enumerate(landmarks.landmark):
-        h, w, _ = image.shape
-        X, Y = int(lm.x * w), int(lm.y * h)
-        imlist.append([id, X, Y])
-
-    for connection in md_pose.POSE_CONNECTIONS:
-        start_landmark = connection[0]
-        end_landmark = connection[1]
-
-        if start_landmark in landmark_indices and end_landmark in landmark_indices:
-            start_point = (int(landmarks.landmark[start_landmark].x * w), int(landmarks.landmark[start_landmark].y * h))
-            end_point = (int(landmarks.landmark[end_landmark].x * w), int(landmarks.landmark[end_landmark].y * h))
-
-            cv2.line(image, start_point, end_point, 
-                color=(255, 255, 255), 
-                thickness=4
-            )
-
-    for id in landmark_indices:
-        lm = landmarks.landmark[id]
-        h, w, _ = image.shape
-        X, Y = int(lm.x * w), int(lm.y * h)
-
-        cv2.circle(image, (X, Y), 
-            radius = 5, 
-            color = (0, 0, 0), 
-            thickness = 4
-        )
