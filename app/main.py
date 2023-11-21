2e49770c696b158db6c36d39d5c8f129f5882d4f
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-21 14:58:19 +0700

    add test option

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ def main():
     parser.add_argument('-I', '--input', choices=['video', 'camera'], required=True, help='Input source (video or camera)')
     parser.add_argument('-A', '--algorithm', choices=['squat', 'pushup'], required=True, help='Exercise recognition algorithm')
     parser.add_argument('-S', '--source', help='Absolute path to the video file (required if input is video)')
+    parser.add_argument('-T', '--test', help='Absolute path to test data file')
 
     args = parser.parse_args()
 
@@ -21,8 +22,10 @@ def main():
             parser.error('--source is required when input is video')
         print(process_camera(args.source, args.algorithm))
     elif args.input == 'camera':
-        print(process_camera(0, args.algorithm))
-
+        if args.test is None:
+            print(process_camera(0, args.algorithm))
+        else:
+            print(process_camera(args.test, args.algorithm))
 
 if __name__ == "__main__":
     main()
